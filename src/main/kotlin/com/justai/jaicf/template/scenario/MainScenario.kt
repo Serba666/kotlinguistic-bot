05dd4995c275fdb8b86c186ca2dcddc62122d4fa
Serba666 <62903918+Serba666@users.noreply.github.com>
2020-11-07 16:32:56 +0300

    add getAirportsFromCity function

--- src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
+++ src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
@@ -8,25 +8,41 @@ import org.apache.commons.csv.CSVParser
 import java.nio.file.Paths
 import java.nio.file.Files
 
-fun readAll(context: BotContext) {
-    val CSV_File_Path = "src/main/kotlin/com/justai/jaicf/template/dictionaries/IATA.csv"
-    // read the file
-    val reader = Files.newBufferedReader(Paths.get(CSV_File_Path))
-    // parse the file into csv values
-    val csvParser = CSVParser(reader, CSVFormat.DEFAULT)
-    for (csvRecord in csvParser) {
-        // Accessing Values by Column Index
-        val code = csvRecord.get(0)
-        if (code == "AAD") {
-            println (csvRecord.get(1))
-            println (csvRecord.get(2))
-            println (csvRecord.get(3))
-            context.session["airport"] = csvRecord.get(1)
-            context.session["country"]= csvRecord.get(2)
-            context.session["city"] = csvRecord.get(3)
-            }
-        }
-}
+ fun getInfoFromCode(context: BotContext, airportCode: String) {
+     val CSV_File_Path = "src/main/kotlin/com/justai/jaicf/template/dictionaries/IATA.csv"
+     // read the file
+     val reader = Files.newBufferedReader(Paths.get(CSV_File_Path))
+     // parse the file into csv values
+     val csvParser = CSVParser(reader, CSVFormat.DEFAULT)
+     for (csvRecord in csvParser) {
+         // Accessing Values by Column Index
+         val code = csvRecord.get(0)
+         if (code == airportCode) {
+             println (csvRecord.get(1))
+             println (csvRecord.get(2))
+             println (csvRecord.get(3))
+             context.session["airport"] = csvRecord.get(1)
+             context.session["country"]= csvRecord.get(2)
+             context.session["city"] = csvRecord.get(3)
+             }
+         }
+ }
+ fun getAirportsFromCity(context: BotContext, city: String) {
+     val CSV_File_Path = "src/main/kotlin/com/justai/jaicf/template/dictionaries/IATA.csv"
+     // read the file
+     val reader = Files.newBufferedReader(Paths.get(CSV_File_Path))
+     // parse the file into csv values
+     val csvParser = CSVParser(reader, CSVFormat.DEFAULT)
+     for (csvRecord in csvParser) {
+         // Accessing Values by Column Index
+         val cities = csvRecord.get(3)
+         var airports = emptyArray<String>()
+         if (cities == city) {
+             airports += csvRecord.get(1)
+             }
+         }
+     return airports
+ }
 
 object MainScenario : Scenario(
     dependencies = listOf(PetrPig)
@@ -43,13 +59,13 @@ object MainScenario : Scenario(
                     say("Приветствую тебя, мой дорогой путешественник!")
                     say("Я помогу определиться с направлением путешествия в зависимости от количества деревянных в твоём кармане.")
                     say("Итак, какой суммой располагаешь?")
-                    readAll(context)
+                    /*getInfoFromCode(context)
                     val airport = context.session["airport"]
                     val country = context.session["country"]
                     val city = context.session["city"]
                     say(
-                        "code : AAD\n airport: $airport \n country: $country \n city: $city "
-                    )
+                    "code : AAD\n airport: $airport \n country: $country \n city: $city "
+                    )*/
                 }
             }
         }
