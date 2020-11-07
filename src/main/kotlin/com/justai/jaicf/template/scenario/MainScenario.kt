c56d318bb20396c78a14eab69f7142eb3f9fd34e
Serba666 <62903918+Serba666@users.noreply.github.com>
2020-11-07 17:09:44 +0300

    remove functions

--- src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
+++ src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
@@ -3,46 +3,7 @@ package com.justai.jaicf.template.scenario
 import com.justai.jaicf.activator.caila.caila
 import com.justai.jaicf.channel.googleactions.actions
 import com.justai.jaicf.model.scenario.Scenario
-import org.apache.commons.csv.CSVFormat
-import org.apache.commons.csv.CSVParser
-import java.nio.file.Paths
-import java.nio.file.Files
 
- fun getInfoFromCode(context: BotContext, airportCode: String) {
-     val CSV_File_Path = "src/main/kotlin/com/justai/jaicf/template/dictionaries/IATA.csv"
-     // read the file
-     val reader = Files.newBufferedReader(Paths.get(CSV_File_Path))
-     // parse the file into csv values
-     val csvParser = CSVParser(reader, CSVFormat.DEFAULT)
-     for (csvRecord in csvParser) {
-         // Accessing Values by Column Index
-         val code = csvRecord.get(0)
-         if (code == airportCode) {
-             println (csvRecord.get(1))
-             println (csvRecord.get(2))
-             println (csvRecord.get(3))
-             context.session["airport"] = csvRecord.get(1)
-             context.session["country"]= csvRecord.get(2)
-             context.session["city"] = csvRecord.get(3)
-             }
-         }
- }
- fun getAirportsFromCity(context: BotContext, city: String) {
-     val CSV_File_Path = "src/main/kotlin/com/justai/jaicf/template/dictionaries/IATA.csv"
-     // read the file
-     val reader = Files.newBufferedReader(Paths.get(CSV_File_Path))
-     // parse the file into csv values
-     val csvParser = CSVParser(reader, CSVFormat.DEFAULT)
-     for (csvRecord in csvParser) {
-         // Accessing Values by Column Index
-         val cities = csvRecord.get(3)
-         var airports = emptyArray<String>()
-         if (cities == city) {
-             airports += csvRecord.get(1)
-             }
-         }
-     return airports
- }
 
 object MainScenario : Scenario(
     dependencies = listOf(PetrPig)
